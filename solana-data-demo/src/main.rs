cb16572ab64f96606fbaea3e4c97c0c5ffc112c5
chenzhuanglan <czlan91@live.cn>
2025-06-13 17:51:31 +0800

    feat(solana-data-demo): 添加 Anchor 相关功能
    - 在 Cargo.toml 中添加 anchor-client 依赖
    - 新增 parse_transaction.rs 示例文件，展示如何使用 Anchor 客户端
    - 添加程序 IDL 文件：example.json 和 red_packet.json
    - 更新 main.rs，集成 Anchor 相关代码

--- solana-data-demo/src/main.rs
+++ solana-data-demo/src/main.rs
@@ -1,9 +1,24 @@
-use solana_client::rpc_client::{GetConfirmedSignaturesForAddress2Config, RpcClient};
-use solana_client::rpc_config::RpcTransactionConfig;
-use solana_sdk::signature::Signature;
-use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey};
+use anchor_client::{
+    Cluster,
+    solana_client::{
+        rpc_client::{GetConfirmedSignaturesForAddress2Config, RpcClient},
+        rpc_config::RpcTransactionConfig,
+    },
+    solana_sdk::{bs58, commitment_config::CommitmentConfig, pubkey::Pubkey, signature::Signature},
+};
+use anchor_lang::{declare_program, AnchorDeserialize};
 use solana_transaction_status_client_types::UiTransactionEncoding;
 use std::str::FromStr;
+use anchor_lang::__private::base64::Engine;
+use anchor_lang::__private::base64::prelude::BASE64_STANDARD;
+use anyhow::anyhow;
+
+const PROGRAM_DATA: &'static str = "Program data: ";
+declare_program!(red_packet);
+use red_packet::events::{
+    ExpiryTimeUpdated, RedPacketClaimed, RedPacketCreated, RedPacketRefunded,
+};
+use crate::red_packet::client::args::CreateRedpacket;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -12,7 +27,7 @@ async fn main() -> anyhow::Result<()> {
     let rpc_client =
         RpcClient::new_with_commitment(rpc_url.to_string(), CommitmentConfig::confirmed());
 
-    let program_id: Pubkey = "HUkozHHVBLsn1w6SDYZa5fkWv3iHG5wjfLXM2b6j7pKC".parse()?;
+    let program_id: Pubkey = "HqSDjxnoR35q8uRMG3LDDvbJ9Hqj4H4bWMPAsBF1hqJq".parse()?;
     // todo 可以通过循环，向前找交易。每次 记录 第一个交易，直到找到一个没有日志的
     let config = GetConfirmedSignaturesForAddress2Config {
         before: None,
@@ -41,9 +56,32 @@ async fn main() -> anyhow::Result<()> {
                 .log_messages
                 .ok_or("No log messages")
                 .map_err(|_| anyhow::anyhow!("No log messages"))?;
-            if logs.join(",").contains(&"Instruction:".to_string()) {
-                // todo 写到日志里面，
-                println!("{:?}", logs);
+
+            println!("{:?}", logs);
+            for log in logs {
+                if log.starts_with(PROGRAM_DATA) {
+                    let data_str = log.strip_prefix(PROGRAM_DATA).unwrap();
+                    // let decoded_data = bs58::decode(data_str).into_vec()?;
+
+                    // 解码 base64
+                    let decoded_data = BASE64_STANDARD.decode(data_str)?;
+
+                    println!("Decoded data (hex): {:?}", &decoded_data);
+
+                    let (discriminator, event_data) = decoded_data.split_at(8);
+                    println!("Event discriminator: {:?}", discriminator);
+                    
+                    
+                    // discriminator 比较
+                    
+                    // 解析事件数据
+                    let event = RedPacketCreated::try_from_slice(event_data)
+                        .expect("Failed to deserialize event");
+
+                    // 输出结果
+                    println!("Decoded Event: {:?}", event);
+
+                }
             }
         }
     }
